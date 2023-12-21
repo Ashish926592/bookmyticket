6357d58c815259dd22da5d3ea988835fb7ea8be1
ashish <ashish4102001@gmail.com>
2023-12-21 11:24:51 +0530

    add

--- bookmyticket/bookmyticket_api/routes/cities.js
+++ bookmyticket/bookmyticket_api/routes/cities.js
@@ -18,41 +18,75 @@ router.get('/', function (req, res, next) {
 
 });
 
-// router.post('/', (req, res, next) => {
-
-//  let data = req.body;
-//  let dbdata = [data.nam   
-    //     "name": "Delhi1",
-    //     "state": "Delhi2"
-    // }e,data.state];
-
-//     db.query('INSERT INTO city(name,state) values(?)',[dbdata], (err, dbResponse) => {
-//         if (!err) {
-//             res.json(dbResponse.rows)
-//         } else {
-//             console.log(err.message);
-//         }
-
-//     });
-
-
-// });
 
 router.post('/', (req, res, next) => {
 
     // let id=req.body.id;
     let name = req.body.name;
     let state = req.body.state;
-    // console.log(dbjson);
-    console.log(`INSERT INTO city(name,state) values (${name},${state}`)
-    db.query(`INSERT INTO city(name,state) values ($1,$2)`,[name,state], (err, dbResponse)=> {
+
+
+    if(name == null && state == null){
+        res.status(404).send({ "insert":"Please enter a name and state"});
+    }else{
+            // console.log(dbjson);
+    // console.log(`INSERT INTO city(name,state) values (${name},${state}`)
+    db.query(`INSERT INTO city(name,state) values ($1,$2)`, [name, state], (err, dbResponse) => {
         if (err) {
             console.log(err);
-            res.send({"insert":"failed to insert"});
+            res.send({ "insert": "failed to insert" });
         } else {
-            res.send({"insert":"sucessfully inserted"})
+            res.send({ "insert": "sucessfully inserted" })
+        }
+    });
+
+    }
+
+});
+
+
+router.put('/update/:id', (req, res, next) => {
+
+    let id = req.params.id;
+    let name = req.body.name;
+    let state = req.body.state;
+  
+
+    db.query(`UPDATE city SET name = $1 , state = $2 WHERE id =$3`,[name, state, id], (err, dbResponse)=> {
+        if (err) {
+            console.log(err);
+            res.send({ "update": "failed to update" });
+        }else {
+            // Check if any rows were affected
+            if (dbResponse.rowCount === 0) {
+              res.status(404).send({ "update":"No matching record found for the provided ID"});
+            } else {
+              res.status(200).send({ "update":`Data updated for ID ${id}`});
+            }
         }
     });
 });
 
+router.delete('/delete/:id',(req,res,next)=>{
+    let id = req.params.id;
+    console.log(id);
+    db.query(`delete from city where id = $1`,[id], (err, dbResponse)=> {
+        if (err) {
+            console.log(err);
+            res.send({"delete":"failed to delete"});
+        } else {
+            if (dbResponse.rowCount === 0) {
+                res.status(404).send({ "delete":"No matching record found for the provided ID"});
+              }else{
+
+                res.send({"delete":"sucessfully deleted"});
+            }
+        }
+    });
+
+})
+
+
 module.exports = router;
+
+
