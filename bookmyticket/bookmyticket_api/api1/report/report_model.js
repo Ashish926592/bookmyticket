345f48e21ff40b379b41a176ab9fa79166d005d3
ashish <ashish4102001@gmail.com>
2023-12-25 10:33:48 +0530

    add

--- bookmyticket/bookmyticket_api/api1/report/report_model.js
+++ bookmyticket/bookmyticket_api/api1/report/report_model.js
@@ -8,15 +8,15 @@ const db = require('./db');
 
 exports.report1 = ((city_name, res) => {
 
-    const query = ` SELECT t1.name as city_name,t5.name as movie_name
+    const query = ` SELECT t1.name AS city_name,t5.name AS movie_name
     FROM city AS t1
     JOIN cinema AS t2 ON t1.id = t2.city_id
     JOIN cinema_hall AS t3 ON t2.id = t3.cinema_id
     JOIN show AS t4 ON t3.id = t4.cinema_hall_id
     JOIN movie AS t5 ON t4.movie_id = t5.id 
-    group by(t1.name,t5.name) 
-    having t1.name = $1
-    order by t1.name;`
+    GROUP BY(t1.name,t5.name) 
+    HAVING t1.name = $1
+    ORDER BY t1.name;`
 
     db.query(query, [city_name], (err, dbResponse) => {
         if (err) {
@@ -40,13 +40,13 @@ exports.report1 = ((city_name, res) => {
 
 exports.report2 = ((cinema_hall, res) => {
 
-    let query = `SELECT distinct t1.name as hall_name,t3.name as movie_name,t2.time,t2.date
-    FROM cinema_hall as t1 
-    JOIN show as t2 ON t1.id = t2.cinema_hall_id
-    JOIN movie as t3 on t2.movie_id = t3.id
-    group by (t1.name,t3.name,t2.time,t2.date)
-    having t1.name= $1
-    order by t1.name,t2.time;`
+    let query = `SELECT DISTINCT t1.name AS hall_name,t3.name AS movie_name,t2.time,t2.date
+    FROM cinema_hall AS t1 
+    JOIN show AS t2 ON t1.id = t2.cinema_hall_id
+    JOIN movie AS t3 ON t2.movie_id = t3.id
+    GROUP BY (t1.name,t3.name,t2.time,t2.date)
+    HAVING t1.name= $1
+    ORDER BY t1.name,t2.time;`
     db.query(query, [cinema_hall], (err, dbResponse) => {
         if (err) {
             console.log(err);
@@ -69,7 +69,7 @@ exports.report2 = ((cinema_hall, res) => {
 // http://localhost:3000/report/movie_name?movie_name=Dangal
 
 exports.report3 = ((movie_name, res) => {
-    let query = ` select * from movie where name = $1`
+    let query = ` SELECT * FROM movie WHERE name = $1`
     db.query(query, [movie_name], (err, dbResponse) => {
         if (err) {
             console.log(err);
@@ -102,10 +102,10 @@ exports.report3 = ((movie_name, res) => {
 
 exports.report4 = ((city_name, movie_name, cinema_name, hall_name, date, res) => {
 
-    let query = `SELECT t1.name as city_name,t2.name as cinema_name,t3.name as cinema_hall_name,
-    t4.name as cinema_hall_section,t9.name as movie_name,
-    t5.number as seat_number,t6.status as status,
-    t7.price as movie_price,t8.date as show_date,t8.time as movie_time
+    let query = `SELECT t1.name AS city_name,t2.name AS cinema_name,t3.name AS cinema_hall_name,
+    t4.name AS cinema_hall_section,t9.name AS movie_name,
+    t5.number AS seat_number,t6.status AS status,
+    t7.price AS movie_price,t8.date AS show_date,t8.time AS movie_time
     FROM city AS t1
     JOIN cinema AS t2 ON t1.id = t2.city_id
     JOIN cinema_hall AS t3 ON t2.id = t3.cinema_id
@@ -115,9 +115,9 @@ exports.report4 = ((city_name, movie_name, cinema_name, hall_name, date, res) =>
     JOIN show_section AS t7 ON t6.show_section_id = t7.id
     JOIN show AS t8 ON t7.show_id = t8.id
     JOIN movie AS t9 ON t8.movie_id = t9.id
-    where t1.name = $1 and t9.name = $2 and t2.name = $3 
-    and t3.name = $4 and t8.date = $5
-    order by t1.name,t9.name,t3.name,t8.time,t5.number`;
+    WHERE t1.name = $1 AND t9.name = $2 AND t2.name = $3 
+    AND t3.name = $4 AND t8.date = $5
+    ORDER BY t1.name,t9.name,t3.name,t8.time,t5.number`;
 
     // console.log(movie_name, hall_name);
 
@@ -149,12 +149,12 @@ exports.report4 = ((city_name, movie_name, cinema_name, hall_name, date, res) =>
 
 exports.report5 = ((res) => {
 
-    let query = `select t1.name as actor_name,count(t3.id) as number_of_movie
-    from actor as t1
-   join movie_cast as t2 on t1.id = t2.actor_id
-   join movie as t3 on t2.movie_id = t3.id 
-   group by t1.name
-    order by number_of_movie desc limit 10;`
+    let query = `SELECT t1.name AS actor_name,count(t3.id) AS number_of_movie
+    FROM actor AS t1
+   JOIN movie_cast AS t2 ON t1.id = t2.actor_id
+   JOIN movie as t3 ON t2.movie_id = t3.id 
+   GROUP BY t1.name
+   ORDER BY number_of_movie DESC LIMIT 10;`
     db.query(query, (err, dbResponse) => {
         if (err) {
             console.log(err);
@@ -198,18 +198,20 @@ exports.report6 = ((year, res) => {
 // the multiplex chain has generated maximum revenues so that they can offer loyalty points
 // to them.
 
+// http://localhost:3000/report/top_ten_customers
+
 exports.report7 = ((res) => {
-    let query = ` select t8.name as city_name,t7.name as cinema_name,t1.name,sum(t4.price) as amount 
-    from customer as t1
-    join booking as t2 on t1.id= t2.customer_id
-    join show_seating_plan as t3 on t2.id = t3.booking_id
-    join show_section as t4 on t3.show_section_id = t4.id
-    join show as t5 on t4.show_id = t5.id
-    join cinema_hall as t6 on t5.cinema_hall_id = t6.id
-    join cinema as t7 on t6.cinema_id = t7.id
-    join city as t8 on t7.city_id = t8.id
-    group by (t1.name,t7.name,t8.name)
-    order by amount desc limit 10`
+    let query = ` SELECT t8.name AS city_name,t7.name AS cinema_name,t1.name,SUM(t4.price) AS amount 
+    FROM customer AS t1
+    JOIN booking AS t2 ON t1.id= t2.customer_id
+    JOIN show_seating_plan AS t3 ON t2.id = t3.booking_id
+    JOIN show_section AS t4 ON t3.show_section_id = t4.id
+    JOIN show AS t5 ON t4.show_id = t5.id
+    JOIN cinema_hall AS t6 ON t5.cinema_hall_id = t6.id
+    JOIN cinema AS t7 ON t6.cinema_id = t7.id
+    JOIN city AS t8 ON t7.city_id = t8.id
+    GROUP BY (t1.name,t7.name,t8.name)
+    ORDER BY amount DESC LIMIT 10`
     db.query(query, (err, dbResponse) => {
         if (err) {
             console.log(err);
@@ -226,18 +228,20 @@ exports.report7 = ((res) => {
 // As a CEO of the multiplex chain I shall be able to view the cinema wise total no. of
 // bookings.
 
+// http://localhost:3000/report/no_of_booking
+
 exports.report8 = ((res) => {
 
-    let query = `select 
-    t8.name as city_name,t7.name as cinema_name,count(t8.id) as no_of_booking
-    from booking as t2
-    join show_seating_plan as t3 on t2.id = t3.booking_id
-    join show_section as t4 on t3.show_section_id = t4.id
-    join show as t5 on t4.show_id = t5.id
-    join cinema_hall as t6 on t5.cinema_hall_id = t6.id
-    join cinema as t7 on t6.cinema_id = t7.id
-    join city as t8 on t7.city_id = t8.id
-    group by (t7.name,t8.name,t3.status)`
+    let query = `SELECT 
+    t8.name AS city_name,t7.name AS cinema_name,count(t8.id) AS no_of_booking
+    FROM booking AS t2
+    JOIN show_seating_plan AS t3 ON t2.id = t3.booking_id
+    JOIN show_section AS t4 ON t3.show_section_id = t4.id
+    JOIN show AS t5 ON t4.show_id = t5.id
+    JOIN cinema_hall AS t6 ON t5.cinema_hall_id = t6.id
+    JOIN cinema AS t7 ON t6.cinema_id = t7.id
+    JOIN city AS t8 ON t7.city_id = t8.id
+    GROUP BY (t7.name,t8.name,t3.status)`
     db.query(query, (err, dbResponse) => {
         if (err) {
             console.log(err);
@@ -254,19 +258,22 @@ exports.report8 = ((res) => {
 // As a CEO of the multiplex chain I shall be able to view the unique customers who have
 // booked tickets with the multiplex chain.
 
+// http://localhost:3000/report/unique_customers
+
 exports.report9 = ((res) => {
-    let query = `select t8.name as city_name,t7.name as cinema_name,t1.name customer_name,count(t2.id) as count_number
-    from customer as t1
-    join booking as t2 on t1.id= t2.customer_id
-    join show_seating_plan as t3 on t2.id = t3.booking_id
-    join show_section as t4 on t3.show_section_id = t4.id
-    join show as t5 on t4.show_id = t5.id
-    join cinema_hall as t6 on t5.cinema_hall_id = t6.id
-    join cinema as t7 on t6.cinema_id = t7.id
-    join city as t8 on t7.city_id = t8.id
-    group by (t2.id,t1.name,t7.name,t8.name)
-    having count(t2.id) = 1
-    order by city_name`
+    let query = `SELECT t8.name AS city_name,t7.name AS cinema_name,
+    t1.name customer_name,COUNT(t2.id) AS count_number
+    FROM customer AS t1
+    JOIN booking AS t2 ON t1.id= t2.customer_id
+    JOIN show_seating_plan AS t3 ON t2.id = t3.booking_id
+    JOIN show_section AS t4 ON t3.show_section_id = t4.id
+    JOIN show AS t5 ON t4.show_id = t5.id
+    JOIN cinema_hall AS t6 ON t5.cinema_hall_id = t6.id
+    JOIN cinema AS t7 ON t6.cinema_id = t7.id
+    JOIN city AS t8 ON t7.city_id = t8.id
+    GROUP BY (t2.id,t1.name,t7.name,t8.name)
+    HAVING COUNT(t2.id) = 1
+    ORDER BY city_name`
     db.query(query, (err, dbResponse) => {
         if (err) {
             console.log(err);
@@ -287,18 +294,18 @@ exports.report9 = ((res) => {
 
 exports.report10 = ((movie_name, hall_name, res) => {
 
-    let query = `select t6.name as cinema_hall_name,t8.name as movie_name,
-t1.name as customer_name
-from customer as t1
-join booking as t2 on t1.id= t2.customer_id
-join show_seating_plan as t3 on t2.id = t3.booking_id
-join seat as t4 on t3.seat_id = t4.id
-join cinema_hall_section as t5 on t4.cinema_hall_section_id = t5.id
-join cinema_hall as t6 on t5.cinema_hall_id = t6.id
-join show as t7 on t6.id = t7.cinema_hall_id
-join movie as t8 on t7.movie_id = t8.id
-group by(t1.name,t6.name,t8.name)
-having t8.name = $1 and t6.name = $2`;
+    let query = `SELECT t6.name AS cinema_hall_name,t8.name AS movie_name,
+t1.name AS customer_name
+FROM customer AS t1
+JOIN booking AS t2 ON t1.id= t2.customer_id
+JOIN show_seating_plan AS t3 ON t2.id = t3.booking_id
+JOIN seat AS t4 ON t3.seat_id = t4.id
+JOIN cinema_hall_section AS t5 ON t4.cinema_hall_section_id = t5.id
+JOIN cinema_hall AS t6 ON t5.cinema_hall_id = t6.id
+JOIN show AS t7 ON t6.id = t7.cinema_hall_id
+JOIN movie AS t8 ON t7.movie_id = t8.id
+GROUP BY(t1.name,t6.name,t8.name)
+HAVING t8.name = $1 and t6.name = $2`;
 
     // console.log(movie_name, hall_name);
 
