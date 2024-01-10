026ecf076eba4d86ef92586f95bccb2b74990901
ashish <ashish4102001@gmail.com>
2024-01-10 17:49:22 +0530

    add

--- bookmyticket/bookmyticket_api/api1/authenticate_api/authenticate_api_middleware.js
+++ bookmyticket/bookmyticket_api/api1/authenticate_api/authenticate_api_middleware.js
@@ -3,7 +3,7 @@ exports.authenticateSession = (req, res, next) => {
   if (req.session && req.session.user) {
       return next();
   } else {
-      return res.status(401).send('Access denied. Not authenticated.');
+    return next(new Error('Access denied. Not authenticated.'));
   }
 };
 
@@ -13,6 +13,6 @@ exports.checkRole = (role) => (req, res, next) => {
   if (req.session.user && req.session.user[0].role === role) {
       return next();
   } else {
-      return res.status(403).send('Access denied. Insufficient permissions.');
+    return next(new Error('Access denied. Insufficient permissions.'));
   }
 };
